717e15b55ca18ac24705c7fc798efc65ea8ec0e2
Kurtmj93 <kurtmj93@gmail.com>
2023-02-14 15:40:46 -0500

    license choices & badge generation with encodeURI, tested & working

--- index.js
+++ index.js
@@ -66,6 +66,26 @@ const questions = [
         type: 'input',
         name: 'credits',
         message: 'Please provide any credits & thank-yous, here:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Under what license is this project?',
+        choices: [ // all of the options from GitHub new repo
+            'None', 
+            'Apache 2.0', 
+            'GNU General Public v3.0', 
+            'MIT', 
+            'BSD 2-Clause Simplified', 
+            'BSD 3-Clause New', 
+            'Boost Software', 
+            'Creative Commons Zero v1.0 Universal', 
+            'Eclipse Public 2.0', 
+            'GNU Affero General Public v3.0', 
+            'GNU General Pubic v2.0', 
+            'GNU Lesser General Public v2.1', 
+            'Mozilla Public 2.0', 
+            'The Unlicense' ]
     }
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+  var encodedLicense = encodeURI(license); // need to encode for html as there are spaces in the license choices
+  return `![License Badge](https://img.shields.io/badge/license-${encodedLicense}-green?style=for-the-badge)`;
+} else { // If there is no license, return an empty string
+  return '';
+}};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `- [License](#license)`
+  } else { // If there is no license, return an empty string
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    
+    This project is licensed under the ${license} license.`
+  } else {
+    return ''; // If there is no license, return an empty string
+  }
+}
 
 // Function to generate markdown for a professional README
 function generateMarkdown(data) {
